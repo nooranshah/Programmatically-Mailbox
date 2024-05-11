91edbdad1578143fe61818893d03d3b3665d4fba
Nooranshah <Nooranshah@DESKTOP-2VGVIJK>
2024-05-11 22:05:37 +0300

    Add project files.

---
+++ API/Controllers/MailBoxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MailBoxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
